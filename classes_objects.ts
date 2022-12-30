17e70092bb4bbf7292e46489cb199a56db52f24b
Selma <90688501+Selma365@users.noreply.github.com>
2022-12-30 21:37:21 +0100

    Update classes_objects.ts

--- classes_objects.ts
+++ classes_objects.ts
@@ -24,3 +24,44 @@ point.drawFunction1();
 
 
 // clasa moze biti HUMAN ali objekat moze biti JOHN, MARRY..
+
+// nacin pravljena instance iznad nije najbolji jer moze biti puno parametara, pa bi bilo puno linija koda
+// zato sluze konstruktori - Metodi koji se koriste kada hocemo da kreiramo instancu classe, oni se nalaze u classi
+
+class Point{
+    x: number;
+    y: number;
+    constructor(x?:number, y?:number){
+        this.x = x; // parametaru iz classe dodjeljujemo vrijednost koju saljemo u construktoru
+        this.y = y; 
+    }
+}
+
+// kreiranje objekta
+let point = new Point(1,2); // dajemo vrijednosti x i y
+let point = new Point() //ako ne znamo pocetne vrijednosti onda, u construktoru napravimo da su x i y opcioni
+
+// access modifiers -> public, private
+// nacin modifikovanja polja u classi
+
+class Point{
+    constructor( private x?:number, private y?:number){
+        this.x = x; // parametaru iz classe dodjeljujemo vrijednost koju saljemo u construktoru
+        this.y = y; 
+    }
+    //getter za citanje vrijednosti
+    get X(){
+        return this.x;
+    }
+    //setter za setovanje vrijednosti
+    set X(value){
+        this.x = value;
+    }
+}
+
+let point = new Point(1,2);
+let x = point.X //citam private vrijednost x
+point.X = 10; //setujemo vr
+
+
+
