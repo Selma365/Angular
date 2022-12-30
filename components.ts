bfbd99eb7c0c5c4505e3434862b45ba2a7f28680
Selma <90688501+Selma365@users.noreply.github.com>
2022-12-30 23:27:28 +0100

    Create components.ts

---
+++ components.ts
@@ -0,0 +1,9 @@
+// angular radi na principu componenti, componente mogu biti reusable .. (componenta se sastoji od: data, html template and logic)
+
+// 1. Create a component
+// 2. Register it in a module
+// 3. Add an element in an Html markup
+
+// second way of making component
+// throught vsc terminal 
+// ng g c nameofcomponent -> angular add 5 files .css html spec.ts and td and module.ts
