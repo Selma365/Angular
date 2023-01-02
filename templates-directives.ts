c25b6d20245d38a1260cfdd03e8ed8a8d493e714
Selma <90688501+Selma365@users.noreply.github.com>
2023-01-02 23:30:27 +0100

    Update and rename templates.ts to templates-directives.ts

---
+++ templates-directives.ts
@@ -0,0 +1,12 @@
+Templates are an important part of Angular because they define the UI of your application. 
+They contain the HTML elements, directives, and binding expressions that make up the UI, and they determine how the UI will look and behave.
+
+
+Templates can also contain directives, which are special markers that tell Angular to apply logic to a part of the template.
+For example, the *ngFor directive tells Angular to repeat a section of the template for each item in an array:
+
+<ul>
+  <li *ngFor="let item of items">{{item}}</li>
+</ul>
+
+This template will render a list of items by repeating the <li> element for each item in the items array.
